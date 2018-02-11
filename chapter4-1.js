12a47a243ae14d507edd7757e0e7c64b5ca2aba1
TJ Sornet <tsornet@gmail.com>
2018-02-10 18:53:35 -0800

    Chapter 4-1 Done

--- chapter4-1.js
+++ chapter4-1.js
@@ -1,4 +1,4 @@
-// Remove Blanks
+// Remove Blanks - DONE
 // Create a function that, given a string, returns all of that string’s contents, but without blanks. 
 // If given the string " Pl   ayTha  tF   u  nkyM  usi    c  ", return "PlayThatFunkyMusic".
 
@@ -8,7 +8,7 @@ function removeBlanks(string){
 
 console.log(removeBlanks(" Pl   ayTha  tF   u  nkyM  usi    c  "))
 
-// String: Get Digits
+// String: Get Digits - DONE
 // Create a JavaScript function that given a string, returns the integer made from the string’s digits. 
 // Given "0s1a3y5w7h9a2t4?6!8?0", the function should return the number 1357924680.
 
@@ -17,16 +17,24 @@ function getDigits(string){
 }
 console.log(getDigits("0s1a3y5w7h9a2t4?6!8?0"));
 
-// Acronyms
+// Acronyms - DONE
 // Create a function that, given a string, returns the string’s acronym (first letters only, capitalized). 
 // Given " there's no free lunch  -  gotta pay yer way. ", return "TNFL-GPYW". 
 // Given "Live from New York, it's Saturday Night!", return "LFNYISN".
 
 function acronym(string){
-    
+    var arr = []
+    arr = string.split(" ")
+    var newString = ""
+    for(var i = 0; i < arr.length; i++){
+        newString += arr[i][0]
+    }
+    return newString.toUpperCase();
 }
 
-// Count Non-Spaces
+console.log(acronym("Live from New York, it's Saturday Night!"))
+
+// Count Non-Spaces - DONE
 // Accept a string and return the number of non-space characters found in the string. 
 // For example, given "Honey pie, you are driving me crazy", return 29 (not 35).
 
@@ -43,9 +51,20 @@ function nonSpaces(string){
 }
 console.log(nonSpaces("Honey pie, you asefge sdeg are driving me crazy"))
 
-// Remove Shorter Strings
+// Remove Shorter Strings - DONE
 // Given a string array and value (length), remove any strings shorter than length from the array.
 
-function revoveShorterString(arr){
-
+function removeShorterString(arr, length){
+    for(var i = 0, j = 0; i < arr.length; i++){
+        if(arr[i].length < length){
+            arr.splice(i, 1)
+            i--
+            j++
+        }
+    }
+    console.log(arr)
+    return arr
 }
+
+myArr = ["hello", "whatsup", "spaghetti", "hi", "one", "two"]
+removeShorterString(myArr, 4)
